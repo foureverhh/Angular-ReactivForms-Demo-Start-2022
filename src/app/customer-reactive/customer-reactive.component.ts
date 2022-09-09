af97f44fe0af60ea19de1aaf3771fea900fbe318
Yuanheng Li <foureverhh@Yuanhengs-Air.localdomain>
2022-09-09 16:04:53 +0200

    setValue for all property value, patchValue for subset for properties value

--- src/app/customer-reactive/customer-reactive.component.ts
+++ src/app/customer-reactive/customer-reactive.component.ts
@@ -14,7 +14,7 @@ customer: Customer = new Customer(); //model for backend
   
 constructor() {
   this.customerForm = new FormGroup({});
- }
+}
 ngOnInit(): void {
 
   //sync state and value with ngForm
@@ -26,6 +26,24 @@ ngOnInit(): void {
   });
 }
 
+populateTestDataSetValue() {
+  //setValue to set value for all properties
+  this.customerForm.setValue({
+    firstName:'Jack',
+    lastName:'Harkness',
+    email:'jack@torchwood.com',
+    sendCategory: false
+  })
+}
+
+populateTestDataPatchValue() {
+  //pathchValue to set value for subset properties
+  this.customerForm.patchValue({
+    firstName:'Jack',
+    lastName:'Harkness',
+    sendCategory: false
+  })
+}
 save(): void {
   console.log(this.customerForm);
   console.log('Saved: ' + JSON.stringify(this.customerForm.value));
